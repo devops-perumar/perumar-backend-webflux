d0666f6604e4d147468fceb9f8b41dcfaa2c45d6
devops-perumar <fcamasca.casa@gmail.com>
2025-09-03 23:11:44 -0500

    chore: consolidate test configuration

--- src/test/java/pe/edu/perumar/perumar_backend/BaseIT.java
+++ src/test/java/pe/edu/perumar/perumar_backend/BaseIT.java
@@ -5,16 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-import pe.edu.perumar.perumar_backend.config.TestProfileConfig;
+import pe.edu.perumar.perumar_backend.config.TestConfig;
 import pe.edu.perumar.perumar_backend.config.TestSecurityConfig;
-import pe.edu.perumar.perumar_backend.DynamoTestConfig;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
 @Import({
-  TestProfileConfig.class,   // mocks de repos + stubs AWS
-  TestSecurityConfig.class,  // seguridad permitAll en test
-  DynamoTestConfig.class     // mock de DynamoDbClient
+  TestConfig.class,         // mocks de repos + stubs AWS
+  TestSecurityConfig.class  // seguridad permitAll en test
 })
 public abstract class BaseIT { }
