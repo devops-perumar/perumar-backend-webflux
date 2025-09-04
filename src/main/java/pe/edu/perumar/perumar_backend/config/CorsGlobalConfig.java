a25879fa84fdbabc7fe1f86427460fd51bc35623
devops-perumar <fcamasca.casa@gmail.com>
2025-09-03 22:41:59 -0500

    feat: externalize cors allowed origins

--- src/main/java/pe/edu/perumar/perumar_backend/config/CorsGlobalConfig.java
+++ src/main/java/pe/edu/perumar/perumar_backend/config/CorsGlobalConfig.java
@@ -1,19 +1,21 @@
 package pe.edu.perumar.perumar_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.lang.NonNull;
 
 @Configuration
 public class CorsGlobalConfig implements WebFluxConfigurer {
+
+    @Value("${cors.allowed-origins:http://localhost:5173}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(
-                "http://localhost:5173",
-                "http://perumar-backend-env.eba-w9v27vmt.us-east-1.elasticbeanstalk.com"
-            )
+            .allowedOrigins(allowedOrigins)
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("Authorization", "Content-Type")
             .allowCredentials(true);
