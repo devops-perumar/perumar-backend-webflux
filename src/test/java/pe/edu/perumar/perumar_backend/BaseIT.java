b2cd27317ec63ea3fce636244b5a869b2ce801d8
devops-perumar <fcamasca.casa@gmail.com>
2025-09-03 22:57:24 -0500

    chore(test): remove duplicate test security config

--- src/test/java/pe/edu/perumar/perumar_backend/BaseIT.java
+++ src/test/java/pe/edu/perumar/perumar_backend/BaseIT.java
@@ -5,12 +5,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import pe.edu.perumar.perumar_backend.config.TestProfileConfig;
+import pe.edu.perumar.perumar_backend.config.TestSecurityConfig;
+import pe.edu.perumar.perumar_backend.DynamoTestConfig;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
 @Import({
-  pe.edu.perumar.perumar_backend.config.TestProfileConfig.class,   // mocks de repos + stubs AWS
-  pe.edu.perumar.perumar_backend.config.TestSecurityConfig.class,  // seguridad permitAll en test
-  pe.edu.perumar.perumar_backend.DynamoTestConfig.class            // mock de DynamoDbClient
+  TestProfileConfig.class,   // mocks de repos + stubs AWS
+  TestSecurityConfig.class,  // seguridad permitAll en test
+  DynamoTestConfig.class     // mock de DynamoDbClient
 })
 public abstract class BaseIT { }
